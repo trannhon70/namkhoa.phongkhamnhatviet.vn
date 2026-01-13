d91bad87e16a7813c194def0d3c3204f5a0cbaa2
trannhon70 <kevintran351996@gmail.com>
2026-01-13 14:09:04 +0700

    update

--- inc/footer.php
+++ inc/footer.php
@@ -4,18 +4,23 @@
              <h5>BẠN CẦN HỖ TRỢ?</h5>
              <span>LIÊN HỆ NGAY VỚI CHÚNG TÔI ĐỂ ĐƯỢC TƯ VẤN</span>
              <div class="footer__support-body-icon">
-                 <amp-img class="footer__support-body-icon-img" src="<?php echo $local ?>/images/icons/icon_dash.webp" height="13px" width="750px" alt="..."></amp-img>
+                 <amp-img class="footer__support-body-icon-img" src="<?php echo $local ?>/images/icons/icon_dash.webp"
+                     height="13px" width="750px" alt="..."></amp-img>
              </div>
              <div class="footer__support-body-row">
                  <a href="<?php echo $local ?>" class="footer__support-body-card">
-                     <amp-img class="footer__support-body-card-img" src="<?php echo $local ?>/images/icons/icon_group_user.webp" height="54px" width="80px" alt="..."></amp-img>
+                     <amp-img class="footer__support-body-card-img"
+                         src="<?php echo $local ?>/images/icons/icon_group_user.webp" height="54px" width="80px"
+                         alt="..."></amp-img>
                      <div class="footer__support-body-text">
                          <h5>028-7776-7777</h5>
                          <div>Tổng đài hỗ trợ khách hàng 24/7</div>
                      </div>
                  </a>
                  <a href="<?php echo $local ?>" class="footer__support-body-card">
-                     <amp-img class="footer__support-body-card-img" src="<?php echo $local ?>/images/icons/icon_group_chat.webp" height="54px" width="80px" alt="..."></amp-img>
+                     <amp-img class="footer__support-body-card-img"
+                         src="<?php echo $local ?>/images/icons/icon_group_chat.webp" height="54px" width="80px"
+                         alt="..."></amp-img>
                      <div class="footer__support-body-text">
                          <h5>Chat với nhân viên</h5>
                          <div>Giải đáp thắc mắc về quy trình</div>
@@ -26,7 +31,8 @@
      </div>
      <div class="footer__contact">
          <div class="footer__contact-left">
-             <amp-img class="footer__contact-right-img" src="<?php echo $local ?>/images/logo/logo2.webp" height="72px" width="300px" alt="..."></amp-img>
+             <amp-img class="footer__contact-right-img" src="<?php echo $local ?>/images/logo/logo2.webp" height="72px"
+                 width="300px" alt="..."></amp-img>
              <div class="footer__contact-left-title">
                  <span>GIỜ LÀM VIỆC</span>
              </div>
@@ -40,21 +46,26 @@
                  <span>THÔNG TIN LIÊN HỆ</span>
              </div>
              <div class="footer__contact-left-card">
-                 <amp-img class="footer__contact-left-card-img" src="<?php echo $local ?>/images/icons/icon_phone_no.webp" height="34px" width="36px" alt="..."></amp-img>
+                 <amp-img class="footer__contact-left-card-img"
+                     src="<?php echo $local ?>/images/icons/icon_phone_no.webp" height="34px" width="36px" alt="...">
+                 </amp-img>
                  <div class="footer__contact-left-card-div">
                      <h5>HOTLINE</h5>
                      <span>028-7776-7777</span>
                  </div>
              </div>
              <div class="footer__contact-left-card">
-                 <amp-img class="footer__contact-left-card-img" src="<?php echo $local ?>/images/icons/icon_location.webp" height="34px" width="36px" alt="..."></amp-img>
+                 <amp-img class="footer__contact-left-card-img"
+                     src="<?php echo $local ?>/images/icons/icon_location.webp" height="34px" width="36px" alt="...">
+                 </amp-img>
                  <div class="footer__contact-left-card-div">
                      <h5>ĐỊA CHỈ</h5>
-                     <span>73 Kinh Dương Vương, P.12, Q.6, TP.HCM</span>
+                     <span>73 Kinh Dương Vương, Phường Phú Lâm, TP.HCM</span>
                  </div>
              </div>
              <div class="footer__contact-left-card">
-                 <amp-img class="footer__contact-left-card-img" src="<?php echo $local ?>/images/icons/icon_mail.webp" height="34px" width="36px" alt="..."></amp-img>
+                 <amp-img class="footer__contact-left-card-img" src="<?php echo $local ?>/images/icons/icon_mail.webp"
+                     height="34px" width="36px" alt="..."></amp-img>
                  <div class="footer__contact-left-card-div">
                      <h5>MAIL</h5>
                      <span>pknhatviet@gmail.com</span>
@@ -62,7 +73,8 @@
              </div>
          </div>
          <div class="footer__contact-right">
-             <amp-img class="footer__contact-right-img" src="<?php echo $local ?>/images/banner/Map.webp" height="390px" width="680px" alt="..."></amp-img>
+             <amp-img class="footer__contact-right-img" src="<?php echo $local ?>/images/banner/Map.webp" height="390px"
+                 width="680px" alt="..."></amp-img>
          </div>
      </div>
      <div class="footer__copyRight">
@@ -83,7 +95,8 @@
                  <div class="footer__mobile-container-left-top">TƯ VẤN TRỰC TUYẾN 24/7</div>
                  <span class="footer__mobile-container-left-bottom">028-7776-7777</span>
                  <div class="footer__mobile-container-left-icon">
-                     <img width="65px" height="auto" src="<?php echo $local ?>/images/logo_mobile/icon_phone_red.png" alt="...">
+                     <img width="65px" height="auto" src="<?php echo $local ?>/images/logo_mobile/icon_phone_red.png"
+                         alt="...">
                  </div>
                  <div class="footer__mobile-container-left-icon-top">
                      <span>GỌI NGAY</span>
@@ -96,26 +109,29 @@
          </div>
      </div>
  </footer>
-<div class="footer_list_icon">
+ <div class="footer_list_icon">
      <div>
-        <a class="footer_icon_happy" href="<?php echo $local ?>" target="_blank">
-            <amp-img src="<?php echo $local ?>/images/logo_mobile/icon_happy.gif" height="50px" width="50px" alt="..."></amp-img>
-        </a>
-    </div>
-     <div style="margin-top:10px">
-        <a class="footer_icon_zalo" href="<?php echo $local ?>" target="_blank">
-            <amp-img src="<?php echo $local ?>/images/logo_mobile/icon_zalo.png" height="50px" width="50px" alt="..."></amp-img>
-            <div class="ping_zalo"></div>
-        </a>
-    </div>
-     <div style="margin-top:10px">
-        <a class="footer_icon_mess" href="<?php echo $local ?>" target="_blank">
-            <amp-img style="margin-left:3px" src="<?php echo $local ?>/images/logo_mobile/icon_message.webp" height="45px" width="45px" alt="..."></amp-img>
-            <div class="ping"></div>
-            <div class="footer_list_icon_number">10</div>
+         <a class="footer_icon_happy" href="<?php echo $local ?>" target="_blank">
+             <amp-img src="<?php echo $local ?>/images/logo_mobile/icon_happy.gif" height="50px" width="50px" alt="...">
+             </amp-img>
          </a>
-    </div>
-</div>
+     </div>
+     <div style="margin-top:10px">
+         <a class="footer_icon_zalo" href="<?php echo $local ?>" target="_blank">
+             <amp-img src="<?php echo $local ?>/images/logo_mobile/icon_zalo.png" height="50px" width="50px" alt="...">
+             </amp-img>
+             <div class="ping_zalo"></div>
+         </a>
+     </div>
+     <div style="margin-top:10px">
+         <a class="footer_icon_mess" href="<?php echo $local ?>" target="_blank">
+             <amp-img style="margin-left:3px" src="<?php echo $local ?>/images/logo_mobile/icon_message.webp"
+                 height="45px" width="45px" alt="..."></amp-img>
+             <div class="ping"></div>
+             <div class="footer_list_icon_number">10</div>
+         </a>
+     </div>
+ </div>
 
  <script async src="<?php echo $local ?>/js/cdn_image.min.js"></script>
  <script src="<?php echo $local ?>/js/jquery-3.7.1.min.js"></script>
